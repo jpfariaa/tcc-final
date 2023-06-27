2db7f6593261a2e654937e51bd42db6ac2c7a7bf
joao.almeida <joao.almeida@sefaz.es.gov.br>
2023-06-27 17:50:21 -0300

    Adicionando o objeto wishlist na pagina para salvar na lista de desejos.

--- src/main/java/com/tcc/comparador/boot/web/controller/ComparadorController.java
+++ src/main/java/com/tcc/comparador/boot/web/controller/ComparadorController.java
@@ -1,19 +1,39 @@
 package com.tcc.comparador.boot.web.controller;
 
+import com.tcc.comparador.boot.domain.WishList;
+import com.tcc.comparador.boot.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/comparador")
 public class ComparadorController {
 
-    @GetMapping("/comparador")
-    public String comparador() {
-        return "comparador/comparar";
+    @Autowired
+    private WishListService service;
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(WishList wishlist, Model model) {
+        model.addAttribute("wishlist", wishlist);
+        return "comparador/cadastro";
     }
 
-    @PostMapping("/comparar")
-    public String comparar() {
-        return null;
+    @PostMapping("/salvar")
+    public String salvar(@Valid WishList wishlist, BindingResult result, RedirectAttributes attr) {
+        if (result.hasErrors()) {
+            return "cadastro";
+        }
+
+        service.salvar(wishlist);
+        attr.addFlashAttribute("success", "Lista de Desejos salva com sucesso.");
+        return "redirect:/comparador";
     }
 }
